2e1259b2d279125440d4319a27fc66b3110ff9d9
mjBerhe <mjberhe123@hotmail.com>
2022-02-10 19:43:57 -0500

    adding init

---
+++ pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from "next";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+
+const Home: NextPage = () => {
+  return <div className="font-bold bg-gray-600">hello</div>;
+};
+
+export default Home;
