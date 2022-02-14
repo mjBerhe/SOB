73ff567a45daec7088e9a105a121de641897fc52
mjBerhe <mjberhe123@hotmail.com>
2022-02-14 14:36:55 -0500

    socket connection

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,27 @@
 import type { NextPage } from "next";
-import Head from "next/head";
-import Image from "next/image";
-import styles from "../styles/Home.module.css";
+import { io } from "socket.io-client";
+import { useEffect } from "react";
 
 const Home: NextPage = () => {
-  return <div className="font-bold bg-gray-600">hello</div>;
+  useEffect((): any => {
+    const socket = io.connect(process.env.BASE_URL, {
+      path: "/api/socketio",
+    });
+
+    socket.on("connect", () => {
+      console.log("socket connected!", socket.id);
+    });
+
+    if (socket) return () => socket.disconnect();
+  }, []);
+
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <button className="outline-none border border-white p-4 rounded-lg">
+        Create Room
+      </button>
+    </div>
+  );
 };
 
 export default Home;
